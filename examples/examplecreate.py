6f31b9aafd230db9d8a2e142c5cddf063730283c
JIAWea <1552937000@qq.com>
2021-02-25 17:25:53 +0800

    example: stop

--- examples/examplecreate.py
+++ examples/examplecreate.py
@@ -1,11 +1,16 @@
+import threading
 import time
 from threading import Thread
 
 import rxbp
+from rxbp.acknowledgement.acksubject import AckSubject
 from rxbp.acknowledgement.continueack import continue_ack
+from rxbp.acknowledgement.stopack import StopAck, stop_ack
+from rxbp.observer import Observer
 from rxbp.schedulers.asyncioscheduler import AsyncIOScheduler
 from rxbp.schedulers.threadpoolscheduler import ThreadPoolScheduler
 from rxbp.testing.tobserver import TObserver
+from rxbp.typing import ElementType
 
 
 def demo1():
@@ -67,7 +72,66 @@ def demo2():
     t1.join()
 
 
+def demo3():
+    class Subscriber(Observer):
+        def __init__(self, stop_num: int):
+            self.received = []
+            self.stop_num = stop_num
+
+            # counts the number of times `on_next` is called
+            self.on_next_counter = 0
+
+        def on_next(self, elem: ElementType):
+            try:
+                values = list(elem)
+            except Exception:
+                return stop_ack
+
+            self.received += values
+
+            self.on_next_counter += 1
+
+            if self.on_next_counter >= self.stop_num:
+                return stop_ack
+
+            return continue_ack
+
+        def on_error(self, err):
+            print("Exception: ", err)
+
+        def on_completed(self):
+            print("Completed")
+
+    def counter(sink):
+        while True:
+            time.sleep(3)
+            print(f"[**client**] received: ", sink.received)
+
+    def work(o, skd):
+        for i in range(8):
+            ack = o.on_next([i])
+            if isinstance(ack, StopAck):
+                # break
+                print("got stopped!")
+
+        o.on_completed()
+
+    source = rxbp.create(work)
+    source = source.pipe(
+        rxbp.op.subscribe_on(ThreadPoolScheduler("")),
+    )
+
+    sink = Subscriber(stop_num=2)
+    source.subscribe(observer=sink)
+
+    t1 = Thread(target=counter, args=(sink, ))
+    t1.start()
+    t1.join()
+
+
 if __name__ == '__main__':
-    demo1()
+    # demo1()
 
     # demo2()
+
+    demo3()
